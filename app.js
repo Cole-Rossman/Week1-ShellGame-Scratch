4f24bdcfe91f550ef5b741ace14f89693b1f4e32
Cole Rossman <colerossman99@gmail.com>
2022-01-25 11:10:41 -0800

    javascript functions imported and dom elements grabbed. eventlisteners added and css updated

--- app.js
+++ app.js
@@ -1,4 +1,48 @@
 // import functions and grab DOM elements
+const buttonOne = document.getElementById('button-one');
+const buttonTwo = document.getElementById('button-two');
+const buttonThree = document.getElementById('button-three');
+
+const ball1Container = document.getElementById('ball1-container');
+const ball2Container = document.getElementById('ball2-container');
+const ball3Container = document.getElementById('ball3-container');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+const ballLocations = ['ball1', 'ball2', 'ball3'];
+
+let correctGuesses = 0;
+let totalGuesses = 0;
+
+buttonOne.addEventListener('click', () => {
+    const ballSpot = Math.floor(Math.random() * 3);
+    const answer = ballLocations[ballSpot];
+    handleGuess(answer, 'ball1');
+});
+
+buttonTwo.addEventListener('click', () => {
+    const ballSpot = Math.floor(Math.random() * 3);
+    const answer = ballLocations[ballSpot];
+    handleGuess(answer, 'ball2');
+});
+
+buttonThree.addEventListener('click', () => {
+    const ballSpot = Math.floor(Math.random() * 3);
+    const answer = ballLocations[ballSpot];
+    handleGuess(answer, 'ball3');
+});
+
+function handleGuess(correctSpot, userGuesses) {
+  
+}
+
+
+
+
+
+
 
 // let state
 
